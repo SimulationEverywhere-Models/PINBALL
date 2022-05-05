b430fce3ea6d0de8715857c61d1eb177cf9434e6
Ben Earle <ben.earle@live.ca>
2022-05-05 09:34:13 -0400

    Add files via upload

---
+++ pinball.bat
@@ -0,0 +1,3 @@
+C:/eclipse/plugins/CD++Builder_1.1.0/internal/simuOrig.exe  -mpinball.ma -lpinball.log -t00:00:30:000 
+rem 
+pause
---
+++ pinballDRW.bat
@@ -0,0 +1,2 @@
+drawlog  -mpinball.ma -cPinball -lpinball.log -w1 -p0 -0 > pinball
+pause
